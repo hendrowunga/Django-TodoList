0e845dcd33e5f557944817174efae9e394e45735
Hendro <hendrowunga073@gmail.com>
2024-05-06 12:40:50 +0700

    add views

--- mysite/todoapp/views.py
+++ mysite/todoapp/views.py
@@ -1,34 +1,49 @@
-from django.shortcuts import render,redirect
-from .models import TodoForm
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import TodoForm
 from .models import Todo
 
-
-
 def home(request):
-    todolist=Todo.objects.all()
-    context={
-        'todolist':todolist,
+    todolist = Todo.objects.all()
+    context = {
+        'todolist': todolist,
     }
-    return render(request,'todoapp/home.html',context)
+    return render(request, 'todoapp/home.html', context)
 
 def add_todo(request):
-    form=TodoForm()
-    if request.method=='POST':
-        FORM=TodoForm(request.POST)
+    form = TodoForm()
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-        
-        context={
-            'form':form
-        }
-        return render(request,'todoapp/todo_from.html',context)
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    # Render form jika metode request bukan POST atau form tidak valid
+    context = {
+        'form': form
+    }
+    return render(request, 'todoapp/todo_form.html', context)
+
+def update_todo(request,pk):
+    item=get_object_or_404(Todo,id=pk)
+    form=TodoForm(instance=item)
+    if request.method=='POST':
+        form=TodoForm(request.POST,instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={
+        'form':form
+    }
+    return render(request,'todoapp/todo_form.html',context)
+
+
+def delete_todo(request,pk):
+    item=get_object_or_404(Todo,id=pk)
+    context= {
+        'item':item,
+    }
+    return render(request,'todoapp/delete_todo.html',context)
+
+def accept_delete_todo(request,pk):
+    todo=get_object_or_404(Todo,id=pk)
+    todo.delete()
+    return redirect('home')
